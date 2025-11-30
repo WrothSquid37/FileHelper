1ffbdeb05ee8db91de901a820d1182560cae96d4
WrothSquid37 <cadeynrauscher@gmail.com>
2025-11-29 17:57:29 -0800

    Fixed git bug

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 use std::fs;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
 
 fn read_from_file(file_path: &str) -> Vec<String> {
-    let file: Result<File, io::Error> = File::open(file_path);
-    let reader: BufReader<Result<File, io::Error>> = BufReader::new(file);
-
+    
     let str_vec: Vec<String>;
-    for line in reader.lines() {
 
-    }
+    
 
     return str_vec;
 
